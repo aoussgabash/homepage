04e58f1caf8466af72f0699e1b400b039f817072
aoussgabash <aouss_gabash@yahoo.com>
2026-08-24 08:40:35 +0200

    Route main website shared components through the central AG loader

--- script.js
+++ script.js
@@ -250,10 +250,10 @@ makeProjectCardLink(
   'Open Gabash Academic Press | فتح دار غباش للنشر الأكاديمي'
 );
 
-if (!document.querySelector('script[data-ag-central-footer]')) {
-  const centralFooter = document.createElement('script');
-  centralFooter.src = 'https://aoussgabash.com/assets/shared/ag-footer.js?v=20260824-1';
-  centralFooter.defer = true;
-  centralFooter.dataset.agCentralFooter = 'true';
-  document.body.appendChild(centralFooter);
+if (!document.querySelector('script[data-ag-loader]')) {
+  const loader = document.createElement('script');
+  loader.src = 'https://aoussgabash.com/assets/shared/ag-loader.js?v=20260824-3';
+  loader.defer = true;
+  loader.dataset.agLoader = 'true';
+  document.body.appendChild(loader);
 }
