89246a0386aa50140fc9163f571da0bc575704bb
aoussgabash <aouss_gabash@yahoo.com>
2026-08-24 08:18:57 +0200

    Add central AG ecosystem configuration

---
+++ assets/shared/ag-config.js
@@ -0,0 +1,30 @@
+window.AG_CONFIG = Object.freeze({
+  version: '2026.08.24.1',
+  owner: {
+    en: 'Dr.-Ing. Aouss Gabash',
+    ar: 'الدكتور المهندس أوس غباش'
+  },
+  ecosystem: {
+    en: 'AG Academic Ecosystem',
+    ar: 'منظومة AG الأكاديمية'
+  },
+  country: {
+    en: 'Syria',
+    ar: 'سوريا'
+  },
+  links: {
+    home: 'https://aoussgabash.com/',
+    siteInformation: 'https://aoussgabash.com/site-information.html',
+    press: 'https://press.aoussgabash.com/'
+  },
+  contact: {
+    email: 'aoussgabash@ieee.org'
+  },
+  footer: {
+    year: 2026,
+    siteInformationLabel: {
+      en: 'Site Information',
+      ar: 'بيانات الموقع'
+    }
+  }
+});
